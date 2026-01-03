a7b0e30f9c16bf16e33f410a3aebe556cbd6a988
dmjDev <dmj.develop@gmail.com>
2026-01-03 12:49:18 +0100

    Update 12:49 3/1/26

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dmjdev-postcode-adress/'
+  base: '/dmjdev-react-postcode-adress/'
 })
